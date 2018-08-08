2ea91ea756e7ae8e559bd484fde99f6c9e57a0d6
pmerienne <pierre.merienne@gmail.com>
2018-08-08 17:04:10 +0200

    Suport loading dataframe from GS sharded files

--- datalab_utils/storage.py
+++ datalab_utils/storage.py
@@ -1,31 +1,37 @@
 import io
+import fnmatch
 import pandas as pd
 
 try:
-    import urlparse
+    from urllib.parse import urlparse
 except ImportError:
-    import urllib.parse as urlparse
+    from urlparse import urlparse
 from google.cloud import storage
 
 
 def read_gs_csv(project_id, uri, **kwargs):
-    parts = urlparse(uri)
-    bucket_name = parts.netloc
-    blob_name = parts.path
-    if blob_name.startswith('/'):
-        blob_name = blob_name[1:]
-
-    client = storage.client.Client(project=project_id)
-    bucket = storage.bucket.Bucket(client, bucket_name)
-    assert bucket, 'Bucket %s not found' % bucket_name
-    blob = bucket.get_blob(blob_name)
-    assert blob, 'Blob %s not found' % uri
-
     print('Loading %s in memory' % uri)
     buffer = io.BytesIO()
-    blob.download_to_file(buffer)
+    for blob in _list_blobs(project_id, uri):
+        blob.download_to_file(buffer)
     buffer.seek(0)
     print('Loading DataFrame')
     df = pd.read_csv(buffer, **kwargs)
     del buffer
     return df
+
+
+def _list_blobs(project_id, uri):
+    parts = urlparse(uri)
+    bucket_name = parts.netloc
+    blob_pattern = parts.path
+    if blob_pattern.startswith('/'):
+        blob_pattern = blob_pattern[1:]
+
+    client = storage.client.Client(project=project_id)
+    bucket = storage.bucket.Bucket(client, bucket_name)
+    assert bucket, 'Bucket %s not found' % bucket_name
+    blobs = [blob for blob in bucket.list_blobs() if fnmatch.fnmatch(blob.name, blob_pattern)]
+    assert blobs, 'No blob found for %s' % uri
+
+    return blobs
